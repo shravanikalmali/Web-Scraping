f6441fceb96aba27770dbbcdd963c5eecbbf3147
shravanikalmali <141416930+shravanikalmali@users.noreply.github.com>
2023-10-23 01:03:21 +0530

    Update web_scraping_wynk_music.py

--- web_scraping_wynk_music.py
+++ web_scraping_wynk_music.py
@@ -2,14 +2,7 @@
 import requests
 import string
 from bs4 import BeautifulSoup
-list_char=[]
-for letter in string.ascii_lowercase:
-   list_char.append(str(letter))
-list_char.remove("w")
-list_char.remove("x")
-
 URL ="https://wynk.in/music/package/trending-in-hindi/amsta_4jf452131595580528558?ref=sub_header"
-myURL=[]
 reqs=[]
 reqs.append(requests.get(URL))
 page0=BeautifulSoup(reqs[0].content,'html5lib')
